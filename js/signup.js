991e13478c97caf00272c653b00c4465d7ba9243
서현준 <hhjr976@gmail.com>
2025-09-08 18:22:06 +0900

    회원가입 후 홈페이지 이름변경 작업 진행 중

--- js/signup.js
+++ js/signup.js
@@ -9,18 +9,17 @@ const wrongMessage = document.querySelector("#wrong-message");
 
 // submit되면 회원가임 검증
 signupForm.addEventListener("submit", event => {
-    // 비밀 번호 유효성 객체 생성
-	const pattern = new RegExp("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}");;
-	
+	// 비밀 번호 유효성 객체 생성
+	const pattern = new RegExp("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}");
 
-    // 브라우저 기본 검증을 먼저 통과시키기
+	// 브라우저 기본 검증을 먼저 통과시키기
 	if (!signupForm.checkValidity()) {
 		return; // 이름, 이메일 입력 안하면 밑에 로직 실행 안하고 종료
 	}
 
-    // 기본 검증을 통과했으면 브라우저 기본 검증 막고 커스텀 검증 수행
-    event.preventDefault();
-    
+	// 기본 검증을 통과했으면 브라우저 기본 검증 막고 커스텀 검증 수행
+	event.preventDefault();
+
 	// 비밀번호 유효성 검증
 	// 적합하지 않으면 아래 안내 문구 붉게 변하고 위 아래 애니메이션
 	if (!pattern.test(userPasswordInput.value)) {
@@ -36,9 +35,9 @@ signupForm.addEventListener("submit", event => {
 		);
 		return; // 비밀번호 검증 실패시 함수 종료
 	} else {
-        patternHelp.classList.remove("text-danger");
+		patternHelp.classList.remove("text-danger");
 		patternHelp.classList.remove("wrong-animation");
-    }
+	}
 
 	// 비밀번호 일치 체크
 	if (userPasswordInput.value !== userPasswordInputCheck.value) {
@@ -48,41 +47,46 @@ signupForm.addEventListener("submit", event => {
 		wrongMessage.classList.add("d-none");
 	}
 
-    // 검증 통화 하면 아래 로직 실행
+	// 검증 통화 하면 아래 로직 실행
 
-    // 환영 페이지 등장
-    signup.classList.add("d-none");
-    welcome.classList.remove("d-none");
+	// 환영 페이지 등장
+	signup.classList.add("d-none");
+	welcome.classList.remove("d-none");
 
-    //localstorage에 저장
-    const userName = document.querySelector("#user-name").value.trim();
-    const userEmail = document.querySelector("#user-email").value.trim();
-    const userPassword = document.querySelector("#user-password").value.trim();
-    userInfoStorage(userName, userEmail, userPassword);
+	//localstorage에 저장
+	const userName = document.querySelector("#user-name").value.trim();
+	const userEmail = document.querySelector("#user-email").value.trim();
+	const userPassword = document.querySelector("#user-password").value.trim();
+	userInfoStorage(userName, userEmail, userPassword);
+
+	//홈 화면 로그인 버튼 없애기
+	const headerLogin = document.querySelector("#headerLogin");
+	const loginUserName = document.querySelector("#loginUser-name");
+
+	headerLogin.classList.add("d-none");
+	loginUserName.classList.remove("d-none");
+	loginUserName.textContent = `안녕하세요 ${userName}님!`;
 });
-
-
 
 // 사용자 정보 localstorage에 저장하는 함수
 function userInfoStorage(userName, userEmail, userPassword) {
-    // 유저 정보 객체 생성
-    const newUser = {
-        userName: userName,
-        userEmail: userEmail,
-        userPassword: userPassword
-    };
-   
-    // localstorage에 있는 유저 정보 불러오고 새 유저 객체 배열에 추가하기
-    
-    let users = JSON.parse(localStorage.getItem("users"))
+	// 유저 정보 객체 생성
+	const newUser = {
+		userName: userName,
+		userEmail: userEmail,
+		userPassword: userPassword
+	};
 
-    if (users) {
-        users.push(newUser);
-        localStorage.setItem("users", JSON.stringify(users))
-    } else {
-        users = [];
-        users.push(newUser);
-        localStorage.setItem("users", JSON.stringify(users))
-    }
+	// localstorage에 있는 유저 정보 불러오고 새 유저 객체 배열에 추가하기
+
+	let users = JSON.parse(localStorage.getItem("users"));
+
+	if (users) {
+		users.push(newUser);
+		localStorage.setItem("users", JSON.stringify(users));
+	} else {
+		users = [];
+		users.push(newUser);
+		localStorage.setItem("users", JSON.stringify(users));
+	}
 }
-
